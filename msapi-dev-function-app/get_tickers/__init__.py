2ef9b1b5c0397f8c41396e3f99b9cda0c0770f39
Tal <tal@biskilled.com>
2023-02-04 18:59:05 +0000

    update

--- msapi-dev-function-app/get_tickers/__init__.py
+++ msapi-dev-function-app/get_tickers/__init__.py
@@ -21,16 +21,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             start_date=req_body.get('start_date')
             end_date=req_body.get('end_date')
     
-    """
+    
     if ticker:
         logging.info ("get_tickers: Ticker:{ticker}, Start_date:{start_date}, End_date:{end_date}")
         api = get_tickers_marketstack()
         res = api.get_tickers_OHLCV(ticker=ticker, start_date=start_date, end_date=end_date)
-        return func.HttpResponse(res)
+        return func.HttpResponse(res, status_code=200)
     
     else:
         err = {"error":"Ticker is not provided"}
         logging.info (err)
         return func.HttpResponse(err, status_code=400)
-    """
-    return func.HttpResponse(err, status_code=200)
