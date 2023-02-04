291d343146785ec983ec6fd77ccfa9ca6ea8fc84
Tal <tal@biskilled.com>
2023-02-04 19:15:44 +0000

    update

--- msapi-dev-function-app/get_ticker_price_changed/__init__.py
+++ msapi-dev-function-app/get_ticker_price_changed/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import json 
 
 import azure.functions as func
 
@@ -27,8 +28,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if ticker:
         logging.info ("get_ticker_price_changed: Ticker:{ticker}, Start_date:{start_date}, End_date:{end_date}")
         data = get_ticker_price_changed(ticker=ticker, start_date=start_date, end_date=end_date)
-        return func.HttpResponse(data, status_code=200)
+        return func.HttpResponse(json.dumps(data), status_code=200)
     else:
         err = {"error":"Ticker is not provided"}
         logging.info (err)
-        return func.HttpResponse(err, status_code=400)
+        return func.HttpResponse(json.dumps(err), status_code=400)
